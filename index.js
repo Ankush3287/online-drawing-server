d883d0592bd19439fcc95655ca80542273049264
Ankush <ankushsolanki3287@gmail.com>
2024-02-05 19:11:31 +0530

    Added undo and redo functionality over web sockets

--- index.js
+++ index.js
@@ -10,6 +10,25 @@ const io = new Server(httpServer, { cors: 'http://localhost:3000' });
 
 io.on("connection", (socket) => {
     console.log("Socket.io Server connected");
+    socket.on('beginPath', (arg)=>{
+        socket.broadcast.emit('beginPath', arg);
+    });
+    socket.on('drawLine', (arg)=>{
+        socket.broadcast.emit('drawLine', arg);
+    });
+    socket.on('changeConfig', (arg)=>{
+        socket.broadcast.emit('changeConfig', arg);
+    });    
+    socket.on('menuClick', (arg)=>{
+        socket.broadcast.emit('menuClick', arg);
+    });
+    socket.on('undoClick', (arg) => {
+        socket.broadcast.emit('undoClick', (arg));
+    });
+    socket.on('redoClick', (arg) => {
+        // Broadcast the event to all connected clients, excluding the sender
+        socket.broadcast.emit('redoClick', (arg));
+    });
 });
 
 httpServer.listen(5000, console.log("Express Server running on port 5000"));
